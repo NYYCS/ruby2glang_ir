b4e306fb09bedf20066b6c924c53313682caa106
Ng Yu Yue <nyystu@gmail.com>
2024-04-20 05:51:07 +0000

    feat: add simple testcases and test driver

--- test/lang_parser/test_class_decl.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-command = os.path.realpath("/app/experiment_2/src/lian/lang/main.py") + " --lang=ruby -debug -print_statements " + os.path.realpath("/app/experiment_2/test/cases/var_decl.rb")
-
-print(command)
-
-os.system(command)
