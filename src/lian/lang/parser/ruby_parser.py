218a753b9cc6a25229942fef779e876ecac1e990
Ng Yu Yue <nyystu@gmail.com>
2024-04-22 06:05:56 +0000

    feat: add call exp

--- src/lian/lang/parser/ruby_parser.py
+++ src/lian/lang/parser/ruby_parser.py
@@ -20,6 +20,7 @@ class Parser(common_parser.Parser):
             "true": self.regular_literal,
             "false": self.regular_literal,
             "array": self.array,
+            "hash": self.hash,
         }
 
         return LITERAL_MAP.get(node.type, None)
@@ -53,11 +54,8 @@ class Parser(common_parser.Parser):
         return self.read_node_text(node)
 
     def array(self, node, statements, replacement):
-        mytype = self.find_child_by_field(node, "type")
-        shadow_type = self.read_node_text(mytype)
-
         tmp_var = self.tmp_variable(statements)
-        statements.append({"new_array": {"type": shadow_type, "target": tmp_var}})
+        statements.append({"new_array": {"type": "", "target": tmp_var}})
 
         index = 0
         for child in node.named_children:
@@ -66,6 +64,25 @@ class Parser(common_parser.Parser):
 
             shadow_child = self.parse(child, statements)
             statements.append({"array_write": {"array": tmp_var, "index": str(index), "source": shadow_child}})
+            index += 1
+
+        return tmp_var
+
+    def hash(self, node, statements, replacement):
+        tmp_var = self.tmp_variable(statements)
+        statements.append({"new_map": {"type": "", "target": tmp_var}})
+
+        index = 0
+        for child in node.named_children:
+            if self.is_comment(child):
+                continue 
+            key = self.find_child_by_field(child, "key")
+            shadow_key = self.parse(key, statements)
+
+            value = self.find_child_by_field(child, "value")
+            shadow_value = self.parse(value, statements)
+
+            statements.append({"map_write": {"map": tmp_var, "key": shadow_key, "value": shadow_value}})
             index += 1
 
         return tmp_var
@@ -120,9 +137,12 @@ class Parser(common_parser.Parser):
         EXPRESSION_HANDLER_MAP = {
             "binary": self.binary_expression,
             "unary": self.unary_expression,
+            "conditional": self.conditional_expression,
+            "case": self.case_expression,
             "assignment": self.assignment_expression,
-            "operator_assignment": self.assignment_expression,
-            "call": self.call_expression
+            "operator_assignment": self.operator_assignment_expression,
+            "call": self.call_expression,
+            "element_reference": self.element_reference_expression,
         }
 
         return EXPRESSION_HANDLER_MAP.get(node.type, None)
@@ -162,7 +182,61 @@ class Parser(common_parser.Parser):
 
         return tmp_var
 
+    def conditional_expression(self, node, statements):
+        condition = self.find_child_by_field(node, "condition")
+        consequence = self.find_child_by_field(node, "consequence")
+        alternative = self.find_child_by_field(node, "alternative")
+
+        condition = self.parse(condition, statements)
+
+        body = []
+        elsebody = []
+        tmp_var = self.tmp_variable(statements)
+
+        expr1 = self.parse(consequence, body)
+        body.append({"assign_stmt": {"target": tmp_var, "operand": expr1}})
+
+        expr2 = self.parse(alternative, elsebody)
+        elsebody.append({"assign_stmt": {"target": tmp_var, "operand": expr2}})
+
+        statements.append({"if": {"condition": condition, "body": body, "elsebody": elsebody}})
+
+        return tmp_var
+
+    def case_expression(self, node, statements):
+        pass
+
     def assignment_expression(self, node, statements):
+        left = self.find_child_by_field(node, "left")
+        right = self.find_child_by_field(node, "right")
+
+        shadow_right = self.parse(right, statements)
+
+        if left.type == "element_reference":
+            shadow_map, shadow_key = self.parse_element_reference(left, statements)
+            statements.append(
+                    {"map_write": {"map": shadow_map, "key": shadow_key, "value": shadow_right}})
+            return shadow_right
+
+        if left.type == "call":
+            name = self.find_child_by_field(left, "method")
+            shadow_name = self.parse(name)
+
+            myobject = self.find_child_by_field(left, "receiver")
+            shadow_object = self.parse_call_receiver(myobject, statements)
+
+            statements.append(
+                    {"field_write": {"receiver_object": shadow_object, "field": shadow_name, "source": shadow_right}})
+                    
+            return shadow_right
+
+        shadow_left = self.read_node_text(left)
+
+        statements.append({"assign_stmt": {"target": shadow_left, "operand": shadow_right}})
+
+        return shadow_left
+
+    def operator_assignment_expression(self, node, statements):
         left = self.find_child_by_field(node, "left")
         right = self.find_child_by_field(node, "right")
         operator = self.find_child_by_field(node, "operator")
@@ -171,16 +245,13 @@ class Parser(common_parser.Parser):
         shadow_right = self.parse(right, statements)
         shadow_left = self.read_node_text(left)
 
-        if not shadow_operator:
-            statements.append({"assign_stmt": {"target": shadow_left, "operand": shadow_right}})
-        else:
-            statements.append({"assign_stmt": {"target": shadow_left, "operator": shadow_operator,
-                                               "operand": shadow_left, "operand2": shadow_right}})
-        return shadow_left
+        statements.append({"assign_stmt": {"target": shadow_left, "operator": shadow_operator,
+                                            "operand": shadow_left, "operand2": shadow_right}})
 
+        return shadow_left
+        
 
     def call_expression(self, node, statements):
-
         name = self.find_child_by_field(node, "method")
         shadow_name = self.parse(name, statements)
 
@@ -188,11 +259,10 @@ class Parser(common_parser.Parser):
         myobject = self.find_child_by_field(node, "receiver")
         
         if myobject:
-            shadow_object = self.parse(myobject, statements)
-
+            shadow_object = self.parse_call_receiver(myobject, statements)
             tmp_var = self.tmp_variable(statements)
             statements.append(
-                {"field_read": {"target": tmp_var, "receiver_object": shadow_object, "field": shadow_name}})
+                {"field_read": {"target": tmp_var, "receiver_object": shadow_object , "field": shadow_name}})
             shadow_name = tmp_var
 
         args = self.find_child_by_field(node, "arguments")
@@ -212,6 +282,14 @@ class Parser(common_parser.Parser):
 
         return self.global_return()
 
+    def element_reference_expression(self, node, statements):
+        tmp_var = self.tmp_variable(statements)
+        shadow_map, shadow_key = self.parse_element_reference(node, statements)
+        statements.append({"map_read": {"target": tmp_var, "map": shadow_map, "key": shadow_key}})
+        return tmp_var
+
+    def case_expression(self, node, statements):
+        pass
 
     def check_statement_handler(self, node):
         STATEMENT_HANDLER_MAP = {
@@ -292,3 +370,30 @@ class Parser(common_parser.Parser):
                                 "name": tmp_var,
                                 "target": shadow_value,
                                 "body": for_body}})
+
+    def parse_call_receiver(self, node, statements):
+        receiver = self.find_child_by_field(node, 'receiver')
+        remaining_content = self.read_node_text(node).split('.')
+
+        receiver_object = remaining_content[0]
+        remaining_content = remaining_content[1:]
+
+        for shadow_name in remaining_content:
+            tmp_var = self.tmp_variable(statements)
+            statements.append(
+                {"field_read": {"target": tmp_var, "receiver_object": receiver_object , "field": shadow_name}})
+            receiver_object = tmp_var
+
+        return receiver_object
+
+
+    def parse_element_reference(self, node, statements):
+        myobject = self.find_child_by_field(node, "object")
+        shadow_object = self.parse(myobject, statements)
+
+        # Ruby grammar.js doesn't put a named field for index
+        # TODO: make this work for multikeys reference
+        key = node.children[2:-1][0]
+        shadow_key = self.parse(key, statements)
+
+        return shadow_object, shadow_key
